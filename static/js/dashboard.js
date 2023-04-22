de1eb2ec3e658d15d8f509d02004518251c85c4b
Andrew Beekhof <andrew@beekhof.net>
2023-04-23 06:53:45 +1000

    Update dashboard.js

--- static/js/dashboard.js
+++ static/js/dashboard.js
@@ -437,7 +437,7 @@ function processBucketXML(text, options) {
 	htmlText = htmlText + "</td><td>&nbsp;&nbsp;&nbsp;</td><td>";    
 	
 	badges.sort(patternVertSort);
-	htmlText = htmlText + createFilteredHorizontalTable(badges, "pattern", null, true, (window.innerWidth-200)/140);
+	htmlText = htmlText + createFilteredHorizontalTable(badges, "pattern", null, true, (window.innerWidth-800)/140);
 	htmlText = htmlText + createFilteredVerticalTable(badges, "platform", null, true);
 	htmlText = htmlText + createFilteredVerticalTable(badges, "version", null, true);
 	htmlText = htmlText + "</td></tr></table>";    
